27d611909e25da1c1f00e40f85005c8cb61dd4ba
pivnati <natanmuleta911@gmail.com>
2024-08-30 19:52:34 -0700

    modify footer by adding the maragin for the all itmes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-green-700 text-white py-6 mt-8">
+    <footer className="bg-green-700 text-white py-6 mt-8 ">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <h4 className="text-lg font-semibold">Library Management System</h4>
-          <p className="text-sm">© 2024 All rights reserved.</p>
+          <h4 className="text-lg font-semibold m-4">Library Management System</h4>
+          <p className="text-sm m-4" >© 2024 All rights reserved.</p>
         </div>
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 m-4" >
           <li className="hover:text-gray-200 cursor-pointer">Privacy Policy</li>
           <li className="hover:text-gray-200 cursor-pointer">Terms of Service</li>
           <li className="hover:text-gray-200 cursor-pointer">Contact Us</li>
