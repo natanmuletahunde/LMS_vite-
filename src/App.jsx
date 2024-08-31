4042faadd9c7ee2aac51023a0e265b0fe6964349
pivnati <natanmuleta911@gmail.com>
2024-08-30 18:27:44 -0700

    change  the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import Header from './Header';
+import BookList from './BookList';
+import AddBook from './AddBook';
+import './App.css';
 
 function App() {
-  
+  const [books, setBooks] = useState([
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    { id: 2, title: '1984', author: 'George Orwell' },
+    { id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee' }
+  ]);
+
+  const addBook = (book) => {
+    setBooks([...books, { ...book, id: books.length + 1 }]);
+  };
 
   return (
-     <div>
-      <h1 className='bg-green-700'>Library Management system </h1>
-     </div>
-  )
+    <div className="min-h-screen bg-gray-100">
+      <Header title="Library Management System" />
+      <div className="container mx-auto p-4">
+        <AddBook addBook={addBook} />
+        <BookList books={books} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
