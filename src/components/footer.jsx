d0641ab7227301aae3a4a566b6a335659e7f8a18
pivnati <natanmuleta911@gmail.com>
2024-08-30 19:46:05 -0700

    modify  the footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-green-700 text-white py-6 mt-8">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <h4 className="text-lg font-semibold">Library Management System</h4>
+          <p className="text-sm">© 2024 All rights reserved.</p>
+        </div>
+        <ul className="flex space-x-4">
+          <li className="hover:text-gray-200 cursor-pointer">Privacy Policy</li>
+          <li className="hover:text-gray-200 cursor-pointer">Terms of Service</li>
+          <li className="hover:text-gray-200 cursor-pointer">Contact Us</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default Footer;
