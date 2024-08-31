19ce210b3b8778a68a7f1530ee721037b9640d27
pivnati <natanmuleta911@gmail.com>
2024-08-30 18:21:10 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+     <div>
+      <h1 className='bg-green-700'>Library Management system </h1>
+     </div>
+  )
+}
+
+export default App
